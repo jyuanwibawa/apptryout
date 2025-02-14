e0b26d4c6b80542da7152a12f9f389fcac2a9cca
jyuanwibawa <jatiyuanamartawibawa@gmail.com>
2025-02-14 18:49:46 +0700

    menambahkanbackend

---
+++ backendbismillah/src/test/java/com/example/backendbismillah/BackendbismillahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendbismillah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendbismillahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
