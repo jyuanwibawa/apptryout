0f3ffae12218f7fd869f9236f8845859f9e257cc
jyuanwibawa <jatiyuanamartwibawa@gmail.com>
2025-02-20 14:24:58 +0700

    update

--- backendbismillah/src/main/java/com/example/controller/CourseController.java
+++ backendbismillah/src/main/java/com/example/controller/CourseController.java
@@ -1,11 +1,16 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.entity.Course;
 import com.example.repository.CourseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
